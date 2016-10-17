3ab069f55f3da9d2cf7c84136ad0583565078f8a
Amy Hwang and Maxwell Cady <amy@>
2016-10-17 10:41:49 -0700

    add conversion functions to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+/* var gallonstoliters = function(gallons) {
+  return gallons * 3.785;
+}
+var gallons = parseInt(prompt("how many gallons?"));
+var liters = gallonstoliters(gallons);
+alert(liters);
+
+var cupToTeaspoon = function(cup) {
+  return cup * 48;
+}
+
+var cup = parseInt(prompt("How many cups?"));
+var teaspoons = cupToTeaspoon(cup);
+alert(teaspoons);
+
+var poundsToGrams = function(pounds) {
+  return pounds * 0.0022;
+};
+var pounds = parseInt(prompt("how many pounds?"));
+var grams = poundsToGrams(pounds);
+alert(grams); */
+
+var ouncesToMilliliter = function(ounces) {
+  return ounces * 29.5735;
+}
+
+var ounces = parseInt(prompt("How many ounces?: "));
+var milliliter = ouncesToMilliliter(ounces);
+alert(milliliter);
